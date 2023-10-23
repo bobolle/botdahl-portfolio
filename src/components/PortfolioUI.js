5bdd564de67b56acbeba40000f3471424031223e
botdahl <botdahlstrom@gmail.com>
2023-10-23 12:51:10 +0200

    css cleanup

--- src/components/PortfolioUI.js
+++ src/components/PortfolioUI.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import './PortfolioUI.css';
 
 export default class PortfolioUI extends React.Component {
     render() {
         return (
-            <div className="PortfolioUI p-4 min-h-screen bg-zinc-400">
+            <div className="p-4 min-h-screen bg-zinc-400">
                 <div className="grid grid-cols-1 gap-4">
-                    <div className="bg-stone-100 rounded-md">Olle Dahlström</div>
-                    <div className="bg-stone-100 rounded-md">Short info</div>
-                    <div className="bg-stone-100 rounded-md">Experience</div>
-                    <div className="bg-stone-100 rounded-md">Education</div>
-                    <div className="bg-stone-100 rounded-md">Projects</div>
+                    <div className="bg-stone-100 rounded shadow-md">
+                        <h2 className="p-4 text-center font-bold text-xl text-stone-600">Olle Dahlström</h2>
+                    </div>
+                    <div className="bg-stone-100 rounded shadow-md">
+                        <h2 className="p-4 text-left font-bold text-l text-stone-600">Short info</h2>
+                    </div>
+                    <div className="bg-stone-100 rounded shadow-md">
+                        <h2 className="p-4 text-left font-bold text-l text-stone-600">Experience</h2>
+                    </div>
+                    <div className="bg-stone-100 rounded shadow-md">
+                        <h2 className="p-4 text-left font-bold text-l text-stone-600">Education</h2>
+                    </div>
+                    <div className="bg-stone-100 rounded shadow-md">
+                        <h2 className="p-4 text-left font-bold text-l text-stone-600">Projects</h2>
+                    </div>
                 </div>
             </div>
         );
