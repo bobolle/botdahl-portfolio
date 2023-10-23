99863279d5bf7048f27b029fd915628dd7d0fc2a
botdahl <botdahlstrom@gmail.com>
2023-10-23 12:14:15 +0200

    minor structure and css improvement

--- src/components/PortfolioUI.js
+++ src/components/PortfolioUI.js
@@ -4,10 +4,14 @@ import './PortfolioUI.css';
 export default class PortfolioUI extends React.Component {
     render() {
         return (
-            <div className="PortfolioUI">
-                <header className="PortfolioUI-header">
-                    <h1>Welcome, {this.props.name}!</h1>
-                </header>
+            <div className="PortfolioUI p-4 min-h-screen bg-zinc-400">
+                <div className="grid grid-cols-1 gap-4">
+                    <div className="bg-stone-100 rounded-md">Olle Dahlström</div>
+                    <div className="bg-stone-100 rounded-md">Short info</div>
+                    <div className="bg-stone-100 rounded-md">Experience</div>
+                    <div className="bg-stone-100 rounded-md">Education</div>
+                    <div className="bg-stone-100 rounded-md">Projects</div>
+                </div>
             </div>
         );
     }
