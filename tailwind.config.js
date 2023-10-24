1ff3bec67dab3cfcbb9780f860e1e9885b8737e3
botdahl <botdahlstrom@gmail.com>
2023-10-24 12:43:26 +0200

    added custom theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
       "./index.html", "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            'botdahl-darkslate': '#27374d',
+            'botdahl-slate': '#526d82',
+            'botdahl-lightslate': '#9db2bf',
+            'botdahl-brightslate': '#dde6ed',
+        },
+    },
   },
   plugins: [],
 }
