96e93d95dec63dad989b3df5623042d713c2955d
botdahl <botdahlstrom@gmail.com>
2023-10-25 12:54:27 +0200

    Added more inforows.

--- src/components/Info/InfoRow.js
+++ src/components/Info/InfoRow.js
@@ -3,12 +3,14 @@ import React from 'react';
 export default class InfoRow extends React.Component {
     render() {
         return(
-            <div className="text-center grid grid-cols-2 mx-4 shadow-md">
-                <div className="bg-white font-bold rounded-l p-2">
+            <div className="grid grid-cols-2 mx-4 shadow-md text-sm">
+                <div className="text-left pl-4 bg-botdahl-700 text-sm text-botdahl-200 rounded-l p-1">
                     {this.props.title}
                 </div>
-                <div className="bg-white rounded-r p-2">
-                    {this.props.answer}
+                <div className="text-right pr-4 bg-botdahl-600 text-sm text-botdahl-100 rounded-r p-1">
+                    <a href={this.props.link} target="_blank" rel="noreferrer">
+                        {this.props.answer}
+                    </a>
                 </div>
             </div>
         );
--- src/components/Info/ShortInfo.js
+++ src/components/Info/ShortInfo.js
@@ -4,11 +4,15 @@ import InfoRow from './InfoRow';
 export default class ShortInfo extends React.Component {
     render() {
         return(
-            <div className="bg-stone-200 rounded shadow-md">
-                <h2 className="p-4 text-center font-bold text-l text-stone-600">Short Info</h2>
-                <div className="grid grid-cols-1 p-4">
-                    <InfoRow title="Location" answer="Södertälje, Sweden" />
+            <div className="bg-botdahl-500 rounded shadow-xl my-2 p-2">
+
+                <div className="grid grid-cols-1 gap-1">
+                    <InfoRow title="Location" answer="Södertälje, Sweden" link="" />
+                    <InfoRow title="Email" answer="botdahlstrom@gmail.com" link="" />
+                    <InfoRow title="LinkedIn" answer="Olle Dahlström" link="https://www.linkedin.com/in/olle-dahlstr%C3%B6m-b15413249" />
+                    <InfoRow title="Github" answer="Botdahl" link="https://www.github.com/botdahl" />
                 </div>
+
             </div>
         );
     }
