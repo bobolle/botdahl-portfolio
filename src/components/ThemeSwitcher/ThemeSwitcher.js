1f980991d879113091429cc978b9886e840b1f2b
botdahl <botdahlstrom@gmail.com>
2023-10-28 14:37:28 +0200

    fine-tuned the themeswitcher and removed it from the page for now.

--- src/components/ThemeSwitcher/ThemeSwitcher.js
+++ src/components/ThemeSwitcher/ThemeSwitcher.js
@@ -2,32 +2,25 @@ import React from 'react';
 
 export default class ThemeSwitcher extends React.Component {
     componentDidMount() {
-        if (('theme' in localStorage)) {
-            if (localStorage.theme === 'dark') {
-                document.documentElement.classList.add('dark');
-            }
+        if (('theme' in localStorage) && localStorage.theme === 'dark') {
+            document.documentElement.classList.add('dark');
         }
     }
 
-    switchMode() {
-        if (!('theme' in localStorage)) {
+    switchTheme() {
+        if (!('theme' in localStorage) || localStorage.theme === 'light') {
                 document.documentElement.classList.add('dark');
                 localStorage.theme = 'dark';
         } else {
-            if (localStorage.theme === 'dark') {
-                document.documentElement.classList.remove('dark');
-                localStorage.theme = 'light';
-            } else {
-                document.documentElement.classList.add('dark');
-                localStorage.theme = 'dark';
-            }
+            document.documentElement.classList.remove('dark');
+            localStorage.theme = 'light';
         }
     }
 
     render() {
         return (
-            <button className="bg-stone-300 p-1 rounded" onClick={this.switchMode}>
-                Switch
+            <button className="bg-stone-300 dark:bg-botdahl-600 p-1 rounded shadow-sm" onClick={this.switchTheme}>
+                Switch Dark/Light
             </button>
         );
     }
