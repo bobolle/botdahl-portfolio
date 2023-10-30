93bae735dc1e7850fb5b5c214f74788f9319b6d8
botdahl <botdahlstrom@gmail.com>
2023-10-30 14:12:51 +0100

    added icons to InfoRows

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import InfoRow from './InfoRow';
+import { HiFlag, HiOutlineMail } from 'react-icons/hi';
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 
 export default class Info extends React.Component {
     render() {
@@ -7,10 +9,10 @@ export default class Info extends React.Component {
             <div className="p-4 bg-accent rounded-xl shadow-lg">
 
                 <div className="grid grid-cols-2 gap-4">
-                    <InfoRow title="Plats" answer="Södertälje, Sweden" link="" />
-                    <InfoRow title="Email" answer="botdahlstrom@gmail.com" link="" />
-                    <InfoRow title="LinkedIn" answer="Olle Dahlström" link="https://www.linkedin.com/in/olle-dahlstr%C3%B6m-b15413249" />
-                    <InfoRow title="Github" answer="Botdahl" link="https://www.github.com/botdahl" />
+                    <InfoRow icon={<HiFlag size={30}/>} title="Plats" answer="Södertälje, Sweden" link="" />
+                    <InfoRow icon={<HiOutlineMail size={30}/>} title="Email" answer="botdahlstrom@gmail.com" link="" />
+                    <InfoRow icon={<AiFillLinkedin size={30}/>} title="LinkedIn" answer="Olle Dahlström" link="https://www.linkedin.com/in/olle-dahlstr%C3%B6m-b15413249" />
+                    <InfoRow icon={<AiFillGithub size={30}/>} title="Github" answer="Botdahl" link="https://www.github.com/botdahl" />
                 </div>
 
             </div>
--- src/components/Info/InfoRow.js
+++ src/components/Info/InfoRow.js
@@ -4,12 +4,11 @@ export default class InfoRow extends React.Component {
     render() {
         return(
             <a href={this.props.link} target="_blank" rel="noreferrer">
-                <div className="p-2 text-center bg-secondarybg rounded-xl shadow-md transition hover:scale-[1.03] duration-200">
-                    <div className="font-bold text-sm text-primary rounded-l p-1">
-                        {this.props.title}
-                    </div>
-                    <div className="text-sm text-secondary">
-                        {this.props.answer}
+                <div className="p-2 bg-secondarybg rounded-xl shadow-md transition hover:scale-[1.03] duration-200">
+                    <div className="inline-block mx-2 text-primary">{this.props.icon}</div>
+                    <div className="inline-block">
+                        <div className="font-bold text-primary">{this.props.title}</div>
+                        <div className="text-secondary">{this.props.answer}</div>
                     </div>
                 </div>
             </a>
