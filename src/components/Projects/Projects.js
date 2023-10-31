3f45b0ea544eec9adf12fa13ed89ac78135b2491
botdahl <botdahlstrom@gmail.com>
2023-10-31 19:15:52 +0100

    now fetches repos from api.github and prints out the projects per auto

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,20 +2,43 @@ import React from 'react';
 import Project from './Project';
 
 export default class Projects extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            projects: [],
+        };
+    }
+
+    componentDidMount() {
+        this.handleProjectInfo();
+    }
+
+    handleProjectInfo() {
+        fetch('https://api.github.com/users/botdahl/repos')
+        .then((response) => (response.json()))
+        .then((data) => {
+            this.setState({
+                projects: data
+            })
+        })
+    }
+
     render() {
         return (
             <div className="bg-accent rounded-xl shadow-lg p-4">
                 <h5 className="text-primary font-bold text-lg">Github Projekt</h5>
                 <div className="grid grid-cols-2 gap-4 text-base-content p-2">
+                   {this.state.projects.map((project) => (
+                        <Project 
+                           id={project.id}
+                           name={project.name}
+                           description={project.description}
+                           link={project.html_url}
+                           language={project.language} 
+                           stars={project.stargazers_count}
+                       />
+                    ))}
             
-                    {/* list of projects */}
-                    <Project name="Botdahl-portfolio" link="https://github.com/botdahl/botdahl-portfolio" description="Portfolio for Github pages made with React and Tailwind." />
-                    <Project name="MongiAttackBar" link="https://github.com/botdahl/MongiAttackBar" description="Weapon swing timer addon for World of Warcraft 1.12.1, calculating and visualizing time remaining for next weapon swing." />
-
-                    <Project name="MongiEnemyCastbar" link="https://github.com/botdahl/MongiEnemyCastbar" description="Calculating and visualizing enemy casting time in World of Warcraft 1.12.1." />
-
-                    <Project name="MongiBG" link="https://github.com/botdahl/MongiBG" description="World of Warcraft 1.12.1 addon for auto-queueing in battlegrounds on Kronos IV and some other QOL features like autoress." />
-
                 </div>
             </div>
         );
