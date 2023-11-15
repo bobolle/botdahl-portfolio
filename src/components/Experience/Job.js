dea281444190403f6d040349556d77c71e4cdfb2
botdahl <botdahlstrom@gmail.com>
2023-11-15 12:23:06 +0100

    relocation year on job component

--- src/components/Experience/Job.js
+++ src/components/Experience/Job.js
@@ -4,13 +4,15 @@ export default class Job extends React.Component {
     render() {
         return(
             <div className="mt-2 p-2 bg-secondarybg rounded-xl shadow-xl">
-                <div className="text-sm text-neutral">
-                    {this.props.year}
-                </div>
+				<div className="flex justify-between">
+					<div className="text-md font-bold text-primary">
+						{this.props.job}
+					</div>
 
-                <div className="text-md font-bold text-primary">
-                    {this.props.job}
-                </div>
+					<div className="text-sm text-neutral">
+						{this.props.year}
+					</div>
+				</div>
 
                 <div className="text-md text-secondary">
                     {this.props.description}
