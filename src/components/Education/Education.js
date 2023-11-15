f8a61f85f41d04ed19e0668f7cdfe7da04cad163
botdahl <botdahlstrom@gmail.com>
2023-11-15 12:12:49 +0100

    'Read more' feature added aswell as more info about the degrees

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import Degree from './Degree';
+import { educationData } from './education-data';
 
 export default class Education extends React.Component {
-    render() {
-        return(
-            <div className="p-4 bg-accent rounded-xl shadow-lg">
-                <h5 className="text-primary font-bold text-lg">Utbildning</h5>
-                <div className="p-2">
+	render() {
+		return(
+        	<div className="p-4 bg-accent rounded-xl shadow-lg">
+				<h5 className="text-primary font-bold text-lg">Utbildning</h5>
+				<div className="p-2">
 
-                    {/* List of education */}
-                    <Degree year="2015-2018" degree="Teknik med inriktning IT" school="Rönninge gymnasium" />
-                    <Degree year="2018-2019" degree="T4" school="Tumba gymnasium"/>
-
-                    <Degree year="2022-2023" degree="Kandidatprogrammet datavetenskap" school="Umeå Universitet"/>
+					{ educationData.map(
+						(degree, key) => (
+							<Degree 
+								key={degree.id}
+								degree={degree.degree}
+								school={degree.school}
+								year={degree.year}
+								description={degree.description}
+								courses={degree.courses}
+							/>
+						)
+					) }
 
                 </div>
             </div>
---
+++ src/components/Education/education-data.js
@@ -0,0 +1,45 @@
+export const educationData = [
+	{
+		"id": 1,
+		"degree": "TE informations- & medieteknik",
+		"year": "2015-2018",
+		"school": "Rönninge gymnasium",
+		"description": "3-års av teknikprogrammet med inriktning informations- och medieteknik på gymnasienivå.",
+		"courses": [
+			"Dator- och nätverksteknik",
+			"Programmering 1",
+			"Gränssnittsdesign",
+			"Webbutveckling 1",
+			"Webbutveckling 2",
+			"Webbserverprogrammering 1"
+		]
+	},
+	{
+		"id": 2,
+		"degree": "T4 - Tilläggsår av teknikprogrammet",
+		"year": "2018-2019",
+		"school": "Tumba gymnasium",
+		"description": "Ett till år av teknik som ger titeln gymnasieingenjör.",
+		"courses": [
+			"Mjukvaru- och gränssnittsdesign",
+			"Mobila applikationer",
+			"Webbserverprogrammering 2",
+			"Webbutveckling 3"
+		]
+	},
+	{
+		"id": 3,
+		"degree": "Kandidatprogrammet datavetenskap",
+		"year": "2022-2023",
+		"school": "Umeå universitet",
+		"description": "Första året av kandidatprogrammet datavetenskap.",
+		"courses": [
+			"Imperativ programmering (C)",
+			"DV0: Datavetenskapligt tänkande",
+			"DV1: Datatyper och datastrukturer",
+			"DV2: Algoritmer och problemlösning",
+			"Interaktionsteknik",
+			"Objektorienterad programmeringsmetodik",
+		]
+	}
+];
