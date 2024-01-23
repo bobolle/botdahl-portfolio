9dcffe7cbe1421c47cd6ed125701b4c95f3575bf
botdahl <botdahlstrom@gmail.com>
2024-01-23 11:52:35 +0100

    info text update

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -4,18 +4,17 @@ import { HiFlag, HiOutlineMail } from 'react-icons/hi';
 import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 
 export default class Info extends React.Component {
-    render() {
-        return(
-            <div className="p-4 bg-accent rounded-xl shadow-lg">
-                <div className="text-primary font-bold text-lg">Info</div>
-                <div className="p-2 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-1">
-                    <InfoRow icon={<HiFlag size={30}/>} title="Plats" description="Södertälje, Sweden" />
-                    <InfoRow icon={<HiOutlineMail size={30}/>} title="Email" description="botdahlstrom@gmail.com" />
-                    <InfoRow icon={<AiFillLinkedin size={30}/>} title="LinkedIn" description="Olle Dahlström" link="https://www.linkedin.com/in/olle-dahlstr%C3%B6m-b15413249" />
-                    <InfoRow icon={<AiFillGithub size={30}/>} title="Github" description="Botdahl" link="https://www.github.com/botdahl" />
-                </div>
-
-            </div>
-        );
-    }
+	render() {
+		return(
+			<div className="p-4 bg-accent rounded-xl shadow-lg">
+				<div className="text-primary font-bold text-lg">Info</div>
+				<div className="p-2 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-1">
+					<InfoRow icon={<HiFlag size={30}/>} title="Plats" description="Södertälje, Sweden" />
+					<InfoRow icon={<HiOutlineMail size={30}/>} title="Email" description="botdahlstrom@gmail.com" />
+					<InfoRow icon={<AiFillLinkedin size={30}/>} title="LinkedIn" description="Olle Dahlström" link="https://www.linkedin.com/in/olle-dahlstr%C3%B6m-b15413249" />
+					<InfoRow icon={<AiFillGithub size={30}/>} title="Github" description="Botdahl" link="https://www.github.com/botdahl" />
+				</div>
+			</div>
+		);
+	}
 }
